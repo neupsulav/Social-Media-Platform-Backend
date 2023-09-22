e25750fe9b684dbfa082ee8993eb2ea9f2b09d3c
Sulav Neupane <neupsulav@gmail.com>
2023-09-22 08:34:00 +0545

    Some changes in get all users API for admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,9 @@ const User = require("../models/user");
 
 // get all users
 const getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find({});
+  const users = await User.find({})
+    .sort({ createdAt: -1 })
+    .select("-password -cpassword");
 
   if (!users) {
     return next(new ErrorHandler("Something went wrong", 400));
