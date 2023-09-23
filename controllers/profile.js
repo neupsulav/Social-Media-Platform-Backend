4e9b6b9b9f98973471b63588766581c15b614ebd
Sulav Neupane <neupsulav@gmail.com>
2023-09-23 15:14:19 +0545

    Added API to get followers and following users list

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,6 +3,7 @@ const ErrorHandler = require("../middlewares/errorHandler");
 const User = require("../models/user");
 const Post = require("../models/post");
 
+// to get our profile data
 const selfProfileData = catchAsync(async (req, res, next) => {
   const profileData = await User.findById({ _id: req.user.userId }).select(
     "-_id name username email image followers following"
@@ -24,4 +25,32 @@ const selfProfileData = catchAsync(async (req, res, next) => {
     .json({ profileData, userPosts, followersCount, followingCount });
 });
 
-module.exports = { selfProfileData };
+// to get the users whom we are following
+const followingList = catchAsync(async (req, res, next) => {
+  const followingList = await User.findById({ _id: req.user.userId }).populate({
+    path: "following",
+    select: "_id name username image",
+  });
+
+  if (followingList.following.length == 0) {
+    res.status(200).send("Currently you are not following any users");
+  }
+
+  res.status(200).json({ list: followingList.following });
+});
+
+// to get the users who are following us
+const followersList = catchAsync(async (req, res, next) => {
+  const followersList = await User.findById({ _id: req.user.userId }).populate({
+    path: "followers",
+    select: "_id name username image",
+  });
+
+  if (followersList.followers.length == 0) {
+    res.status(200).send("Currently you are not being followed by anyone");
+  }
+
+  res.status(200).json({ list: followersList.followers });
+});
+
+module.exports = { selfProfileData, followingList, followersList };
--- routers/profile.js
+++ routers/profile.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 const authentication = require("../middlewares/authentication");
 
-const { selfProfileData } = require("../controllers/profile");
+const {
+  selfProfileData,
+  followingList,
+  followersList,
+} = require("../controllers/profile");
 
 // routes
 router.get("/profile", authentication, selfProfileData);
+
+router.get("/profile/following", authentication, followingList);
+
+router.get("/profile/followers", authentication, followersList);
 
 module.exports = router;
